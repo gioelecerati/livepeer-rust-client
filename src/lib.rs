9b8494083dd8417d2d252e70df9a5a1981f3237e
gioelecerati <gioele@cerati.org>
2022-11-01 20:07:25 +0100

    change sin rtmp

--- src/lib.rs
+++ src/lib.rs
@@ -72,7 +72,7 @@ impl LivepeerClient {
             Some(LivepeerEnv::Stg) => "rtmp://rtmp.livepeer.monster:11935/live",
             Some(LivepeerEnv::Prod) => "rtmp://rtmp.livepeer.com/live",
             Some(LivepeerEnv::Origin) => "rtmp://rtmp.livepeer.com/live",
-            Some(LivepeerEnv::Sin) => "rtmp://sin-playback.lp-playback.studio/live",
+            Some(LivepeerEnv::Sin) => "rtmp://sin-rtmp.livepeer.com/live",
             None => "rtmp://rtmp.livepeer.monster:11935/live",
         };
 
