89782f412b009bb0102b587d70469bb97c1968c1
gioelecerati <gioele@cerati.org>
2024-08-14 17:03:01 +0200

    add multipart utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,6 +2,9 @@ use crate::errors;
 use async_std;
 use serde_json;
 use surf::StatusCode;
+use reqwest;
+use tokio;
+use reqwest::multipart;
 
 pub struct SurfRequest {}
 
@@ -77,9 +80,6 @@ impl SurfRequest {
     }
 }
 
-use reqwest;
-use tokio;
-
 pub struct ReqwestRequest {}
 
 impl ReqwestRequest {
@@ -92,6 +92,7 @@ impl ReqwestRequest {
         let req_client = reqwest::Client::new();
         let mut req = req_client.request(method, &url)
             .header("Authorization", format!("Bearer {}", client.config.api_token));
+
 
         if let Some(body) = body {
             req = req.header("Content-Type", "application/json").body(body);
@@ -143,5 +144,35 @@ impl ReqwestRequest {
         client: crate::LivepeerClient,
     ) -> Result<serde_json::Value, errors::Error> {
         tokio::runtime::Runtime::new().unwrap().block_on(Self::make_request(reqwest::Method::DELETE, url, None, client))
+    }
+
+    pub fn post_multipart(
+        url: String,
+        form: multipart::Form,
+        client: crate::LivepeerClient,
+    ) -> Result<serde_json::Value, errors::Error> {
+        tokio::runtime::Runtime::new().unwrap().block_on(async {
+            let req_client = reqwest::Client::new();
+            let req = req_client.post(&url)
+                .header("Authorization", format!("Bearer {}", client.config.api_token))
+                .multipart(form);
+
+            let response = req.send().await.map_err(|_| errors::Error::UNKNOWN)?;
+
+            match response.status() {
+                reqwest::StatusCode::OK | reqwest::StatusCode::CREATED | reqwest::StatusCode::NO_CONTENT => {
+                    if response.status() == reqwest::StatusCode::NO_CONTENT {
+                        Ok(serde_json::Value::Null)
+                    } else {
+                        let text = response.text().await.map_err(|_| errors::Error::UNKNOWN)?;
+                        serde_json::from_str(&text).map_err(|_| errors::Error::UNKNOWN)
+                    }
+                }
+                _ => {
+                    let err = errors::Error::from_reqwest_response(&response);
+                    Err(err)
+                }
+            }
+        })
     }
 }
